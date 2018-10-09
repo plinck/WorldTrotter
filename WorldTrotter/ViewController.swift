8497d8adc22d4bb68b1274791f49c0401a10bb34
Paul Linck <paul.linck@gmail.com>
2018-10-09 15:26:26 -0400

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Paul Linck on 10/9/18.
+//  Copyright © 2018 Paul Linck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
